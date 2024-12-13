0e0053a76011a9876e1b591524f0cd00148e6e45
MarekLuk <161015035+MarekLuk@users.noreply.github.com>
2024-12-13 11:21:15 +0100

    Add files via upload

---
+++ config/corsConfig.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const allowedOrigins = process.env.NODE_ENV === 'production'
+    ? [process.env.PROD_ORIGIN].filter(Boolean)
+    : [process.env.DEV_ORIGIN].filter(Boolean);
+
+module.exports = allowedOrigins;
---
+++ config/db.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require("sequelize");
+require('dotenv').config();
+const createDatabaseIfNotExists = require("../utils/createDatabase")
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+});
+
+module.exports = { sequelize, createDatabaseIfNotExists };
