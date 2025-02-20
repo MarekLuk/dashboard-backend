901792ad43b36c1244d61ba12bbbd1c53c8b34bb
marek.lukaszcz@gmail.com <marek.lukaszcz@gmail.com>
2025-02-20 13:27:58 +0100

    added new deployment

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config();
-const createDatabaseIfNotExists = require("../utils/createDatabase")
+require("dotenv").config();
+const createDatabaseIfNotExists = require("../utils/createDatabase");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false,
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     logging: false,
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+	dialect: "postgres",
+	logging: false,
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 module.exports = { sequelize, createDatabaseIfNotExists };
--- utils/createDatabase.js
+++ utils/createDatabase.js
@@ -2,17 +2,25 @@ const { Client } = require("pg");
 const logger = require("./logger");
 require("dotenv").config();
 
-async function createDatabaseIfNotExists() {
-	const client = new Client({
-		user: process.env.DB_USER,
-		host: process.env.DB_HOST,
-		password: process.env.DB_PASSWORD,
-		port: process.env.DB_PORT,
-		database: "postgres",
-	});
+// async function createDatabaseIfNotExists() {
+// 	const client = new Client({
+// 		user: process.env.DB_USER,
+// 		host: process.env.DB_HOST,
+// 		password: process.env.DB_PASSWORD,
+// 		port: process.env.DB_PORT,
+// 		database: "postgres",
+// 	});
 
+async function createDatabaseIfNotExists() {
+	const connectionString = process.env.DATABASE_URL;
+	const client = new Client({
+		connectionString,
+		ssl: { rejectUnauthorized: false }, // Enable SSL if required by Render
+		database: "postgres", // Connect to the maintenance database to check for creation
+	});
 	try {
 		await client.connect();
+		logger.info("Connected to PostgreSQL maintenance database.");
 		const res = await client.query(
 			`SELECT 1 FROM pg_database WHERE datname = $1`,
 			[process.env.DB_NAME]
